29725b365d684f62267a8124036a86376e0c0c7e
Rohan Gaikwad <gaikwadvrohan@gmail.com>
2022-11-21 21:32:54 +0530

    interface

--- index.ts
+++ index.ts
@@ -1,3 +1,40 @@
 export { ObjKeysToLowerCase, SVGImageURI } from "./utils";
 
 export default {}
+
+declare global {
+    interface Window {
+        languageuid: string;
+        websiteguid: string;
+        WebsiteURL: string;
+        SiteCssVersion: string;
+        tokens: any;
+        cdnURL: string;
+        cookiedetails: string;
+        UserGuid: string;
+        SessionGuid: string;
+        currencysymbol: string;
+        defaultlanguageguid: string;
+        defaultcurrencyguid: string;
+        currencyguid: string;
+        SaaS_Configuration_Microservice_URL: string;
+        SaaS_GlobalElements_Microservice_URL: string;
+        SaaS_ReviewManagement_Microservice_URL: string;
+        SaaS_Product_Microservice_URL: string;
+        SaaS_HTMLConfigurator_Microservice_URL: string;
+        SaaS_VirtualSample_Microservice_URL: string;
+        SaaS_Users_Microservice_URL: string;
+        SaaS_User_Microservice_URL: string;
+        SaaS_Basket_Microservice_URL: string;
+        SaaS_Order_Microservice_URL: string;
+        SaaS_Analytics_Microservice_URL: string;
+        SaaS_Catalog_Microservice_URL: string;
+        SaaS_Freight_Microservice_URL: string;
+        SaaS_ERPIntegration_Microservice_URL: string;
+        SaaS_ProductImportExport_Microservice_URL: string;
+        SaaS_Product_GlobalElements_Microservice_URL: string;
+        SaaS_ProductListing_Microservice_URL: string;
+        SaaS_ProductAdmin_Microservice_URL: string;
+        SaaS_ProductDetails_Microservice_URL: string;
+    }
+}
